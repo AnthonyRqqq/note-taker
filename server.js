d0fdfd374aa1975b9b4d08fea4d3c1b3f021d218
unknown <anthony_rosati@yahoo.com>
2023-12-08 22:33:09 -0600

    Added code to allow post method for new notes

--- server.js
+++ server.js
@@ -24,12 +24,51 @@ app.get('/notes', (req, res) => {
 })
 
 app.get('/api/notes', (req, res) => {
-    res.json(database);
-    console.log(database);
+    res.status(200).json(database);
+    // console.log(database);
 });
 
 app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request was recieved`);
 
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            review_id: uuid()
+        };
+
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                const parsedNotes = JSON.parse(data);
+
+                parsedNotes.push(newNote);
+
+                fs.writeFile(
+                    './db/db.json',
+                    JSON.stringify(parsedNotes, null, 4),
+                    (writeErr) =>
+                      writeErr
+                        ? console.error(writeErr)
+                        : console.info('Successfully updated notes!')
+                  );
+            }
+        })
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        console.log(response);
+        res.status(201).json(response);
+    } else {
+        res.status(500).json('Error in adding new note.')
+    }
 });
 
 app.delete('/api/notes', (req, res) => {
